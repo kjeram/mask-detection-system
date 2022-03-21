3e0c3f13484987692e1ebc24818c1d58a4990996
Kjetil Rambjørg <rakj0804@gmail.com>
2022-03-21 15:58:42 +0100

    updated face detection model

--- main.py
+++ main.py
@@ -1,138 +1,122 @@
-from time import sleep
 import cv2
 import os
-import shutil
 import numpy as np
-from tensorflow.keras.models import load_model
+import time
 
-
-#-- Data Processer --#
-
-def center_crop_image(img):
-    h, w = img.shape[:2]
+def center_box(box):
+    startX, startY, endX, endY = box.astype("int")
+    h = endY - startY
+    w = endX - startX
     aspect = h/w
     if aspect > 1:
         offset = int(np.round((h / 2) - (w / 2)))
-        return img[offset:w + offset, 0:w]
+        return startX - offset, startY, endX + offset, endY
     else:
         offset = int(np.round((w / 2) - (h / 2)))
-        return img[0:h, offset:h + offset]
+        return startX, startY - offset, endX, endY + offset
 
-def process_images(images, image_size):
-    cropped_images = [center_crop_image(img) for img in images]
-    resized_images = [cv2.resize(img, (image_size, image_size), cv2.INTER_AREA) for img in cropped_images]
-    return resized_images
+def classify_image(interpreter, image):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    input_data = image
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    return output_data
 
-
-#-- Image processing methods --#
-
-def convertToRGB(img):
-    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-def detect_faces(f_cascade, colored_img, scaleFactor = 1.1):
-    #just making a copy of image passed, so that passed image is not changed
-    img_copy = colored_img.copy()
-    
-    #convert the test image to gray image as opencv face detector expects gray images
-    gray = cv2.cvtColor(img_copy, cv2.COLOR_RGB2GRAY)
-    
-    #let's detect multiscale (some images may be closer to camera than others) images
-    faces = f_cascade.detectMultiScale(gray, scaleFactor=scaleFactor, minNeighbors=5)
-    
-    #go over list of faces and draw them as rectangles on original colored img
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img_copy, (x, y), (x+w, y+h), (0, 255, 0), 1)
-        
-    return img_copy
-
-def crop_face(f_cascade, img, scaleFactor = 1.1):
-    # convert the image to gray image as opencv face detector expects gray images
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # let's detect multiscale (some images may be closer to camera than others) images
-    faces = f_cascade.detectMultiScale(gray, scaleFactor=scaleFactor, minNeighbors=5)
-    try:
-        x, y, w, h = faces[0]
-    except IndexError:
-        return None
-    return img[y:y+h, x:x+w]
-
-
-# Main starts here
-
-print('[INFO] Starting')
+# Set to True if running on Raspberry Pi
+arm = False
 
 # Set working dir
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # Face detect model
-face_detect = 'data\haarcascades\haarcascade_frontalface_alt2.xml'
-assert os.path.exists(face_detect)
-haar_face_cascade = cv2.CascadeClassifier(face_detect)
+prototxt_path = 'deploy.prototxt'
+weights_path = 'res10_300x300_ssd_iter_140000_fp16.caffemodel'
+face_detect = cv2.dnn.readNet(prototxt_path, weights_path)
 
 # CNN
-model_folder = 'model'
-assert os.path.exists(model_folder)
-model = load_model(model_folder)
+if arm:
+    from tflite_runtime.interpreter import Interpreter
+    model = Interpreter('model.tflite')
+    model.allocate_tensors()
+else: 
+    from tensorflow.keras.models import load_model
+    model_folder = 'model'
+    model = load_model(model_folder)
 
 # Image properties
 image_size = 96
-extensions = ['jpg', 'jpeg', 'png']
 
-# Folders
-source_folder = 'source/'
-dest_folder = 'images/'
-copy_folder = 'back/'
-if not os.path.exists(source_folder):
-    os.makedirs(source_folder)
-if not os.path.exists(dest_folder):
-    os.makedirs(dest_folder)
-if not os.path.exists(copy_folder):
-    os.makedirs(copy_folder)
+# Camera init
+video_capture = cv2.VideoCapture(0)
+ret, frame = video_capture.read()
 
 # Main loop
-print('[INFO] Main loop starts')
+while True:
+    # Start timer
+    time1 = time.time()
 
-try:
-    while True:
-        valid_files = [x for x in os.listdir(source_folder)]
-        # If files in source folder
-        if os.listdir(source_folder) != 0:
-            # Iterate through all files
-            for i in os.listdir(source_folder):
+    # Read frame from camera
+    _, frame = video_capture.read()
+    h, w = frame.shape[:2]
 
-                # Try to move files to "copy folder"
-                while True:
-                    try:
-                        shutil.move(source_folder + i, copy_folder + i)
-                        break
-                    except PermissionError:
-                        sleep(2)
-                
+    # OpenCV DNN pre-processing
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (image_size * 2, image_size * 2))
+    # Process the pre-processed frame & find faces
+    face_detect.setInput(blob)
+    faces = face_detect.forward()
 
-                # Get the image data
-                img_raw = crop_face(haar_face_cascade, cv2.imread(copy_folder + i))
+    # For all faces detected in frame
+    for i in range(0, faces.shape[2]):
+        # Get the confidence that it is a face 
+        confidence = faces[0, 0, i, 2]
+        if confidence > 0.7:
+            # Get coordinates for face
+            box = faces[0, 0, i, 3:7] * np.array([w, h, w, h])
+            startX, startY, endX, endY = center_box(box)
 
-                # Crop & resize image
-                img_list = process_images([img_raw], image_size)
+            # ensure the bounding boxes fall within the dimensions of the frame
+            startX, startY = max(0, startX), max(0, startY)
+            endX, endY = min(w - 1, endX), min(h - 1, endY)
 
-                # Reshape image structure
-                img = np.array(img_list).reshape(-1, image_size, image_size, 3)
+            # Pre-process frame
+            frame_crop = frame[startY:endY, startX:endX]
+            frame_crop_resize = cv2.resize(frame_crop, (image_size, image_size), cv2.INTER_AREA)
+            frame_crop_resize_reshape = np.array(frame_crop_resize, dtype=np.float32).reshape(-1, image_size, image_size, 3)
 
-                # Make a prediction
-                pred = model.predict(img)
+            # Make a prediction
+            if arm:
+                pred = classify_image(model, frame_crop_resize_reshape)
+            else:
+                pred = model.predict(frame_crop_resize_reshape)
 
-                # Convert prediction to lables
-                if pred[0].argmax(axis=0) == 0:
-                    label = 'Mask'
-                else:
-                    label = 'No_Mask'
-                
-                # Write image to folder
-                cv2.imwrite(dest_folder + label + '_' + i, img_list[0])
-                print(f'[INFO] Predicted {i}')
-        # If no file are found
-        else:
-            sleep(5)
-except KeyboardInterrupt:
-    print('[INFO] Exiting')
-    exit()
+            # Convert prediction to lables
+            if pred[0].argmax(axis=0) == 0:
+                label = 'Mask'
+                color = (0,255,0)
+                acc = pred[0][0]
+            else:
+                label = 'No Mask'
+                color = (0,0,255)
+                acc = pred[0][1]
+
+            # Draw a rectangle around the faces
+            text = label + ' (' +  str(round(acc * 100)) + '%)'
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+            cv2.putText(frame, text, (startX, startY - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 1, cv2.LINE_AA)
+
+    # Stop timer & calculate execution time
+    time2 = time.time()
+    classification_time = np.round(time2-time1, 3)
+    cv2.putText(frame, str(classification_time), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 1, cv2.LINE_AA)
+
+    # Display the resulting frame
+    cv2.imshow('window', frame)
+    
+    # Key code for ESC
+    if cv2.waitKey(1) & 0xFF == 27:  
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
